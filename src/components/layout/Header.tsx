f023f4829b03cbcca2e69a6c2d1eb20e1bfd58a7
横田昴輝 <kokiyokotakoki@gmail.com>
2025-03-10 02:22:52 +0900

    style: 既存の要素にレスポンシブ対応追加

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./header.module.css";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
+    <header>
       <Link href="/">
         <h1 className={styles.h1}>Teck Challenge</h1>
       </Link>
