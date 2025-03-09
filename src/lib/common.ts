521bf3ff449eceb75ef52265c2f5d08228d2a015
横田昴輝 <kokiyokotakoki@gmail.com>
2025-03-09 20:55:31 +0900

    test: 動作確認済みのため、不要になったコード削除

--- src/lib/common.ts
+++
@@ -1,4 +0,0 @@
-export function getLabelFromOption(options: { value: number; label: string }[], value: number): string {
-  const target = options.find(option => option.value === value);
-  return target?.label ?? "";
-}
