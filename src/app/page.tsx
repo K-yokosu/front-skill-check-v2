7a279803cf915855bc3e1760e4baf43a6be9d2db
横田昴輝 <kokiyokotakoki@gmail.com>
2025-03-12 01:51:37 +0900

    feat: 天気予報api実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import LocationInput from "@/features/top/location/comoponents/LocationInput/LocationInput";
-
+import LocationInput from "@/features/weather/comoponents/LocationInput/LocationInput";
+import { getForecastWeather } from "@/features/weather/api/forecastWeather";
+import { WeatherForecastType } from "@/features/weather/types/weather";
 type SearchParams = Promise<{ [key: string]: string | undefined }>;
 
 export default async function Page(props: { searchParams: SearchParams }) {
   const searchParams = await props.searchParams;
   const q = searchParams.q ?? "";
+  const weatherLists: WeatherForecastType | undefined = await getForecastWeather(q);
+  console.log(weatherLists);
 
   return (
     <>
